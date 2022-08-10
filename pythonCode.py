1548919399168330eeb406e50ec577fd861ea8c5
LordBowser0 <87528635+LordBowser0@users.noreply.github.com>
2022-08-10 17:54:38 +0200

    Add files via upload

---
+++ pythonCode.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import plotly.express as px
+
+print('Hello World')
+df = pd.read_csv('D:\Project\Housing.csv')
+df.sort_values(by='price')
+print(df)
+fig = px.line(df, x='price', y='area', title='area dependent on price ')
+fig.show()
+
+
+def importdata(data):
+    df = pd.read_csv(data)
+    print(df)
+
+
+def scatterplot(xaxis, yaxis, name):
+    plot = px.scatter(df, x=xaxis, y=yaxis, title=name)
+    plot.show()
+
+
+def lineplot(xaxis, yaxis, name):
+    df.sort_values(by=xaxis)
+    plot = px.line(df, x=xaxis, y=yaxis, title=name)
+    plot.show()
